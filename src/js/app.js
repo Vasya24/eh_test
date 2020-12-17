d3d72252972b9cdbec4b6ef9f39d57ead8961760
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-12-17 17:22:50 +0300

    Linter's warnings fixed

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
-//Cancelling form default submitting
-let bookForm = document.querySelector('form[data-id="dates-form"]');
+// Cancelling form default submitting
+const bookForm = document.querySelector('form[data-id="dates-form"]');
 bookForm.addEventListener('submit', (e) => {
-  e.preventDefault()
-})
-
-let labels = document.querySelectorAll('input[name="type"] + label');
-
+  e.preventDefault();
+});
