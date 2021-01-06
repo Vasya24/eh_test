e195db8a21b536908efd2ca0b80128c50ae93624
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2021-01-06 11:44:47 +0300

    Swiper done. Top dropdown done 80%

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,11 @@ const { forms } = document;
 for (const f of forms) {
   f.onsubmit = (e) => { e.preventDefault(); };
 }
+const drop = document.querySelector('.dropdown');
+const dropLink = document.querySelector('.header-link:nth-child(1)');
+dropLink.onmouseover = () => {
+  drop.classList.add('active')
+}
+drop.onmouseleave = () => {
+  drop.classList.remove('active')
+}
