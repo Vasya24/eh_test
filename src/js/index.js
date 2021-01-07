9fcea4db992cdf7653e5ac92d494fd4ac93c5af3
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2021-01-07 19:15:02 +0300

    Top for portrait tablets done

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,37 @@
+// Removing default behavior for forms
 const { forms } = document;
 
 for (const f of forms) {
-  f.onsubmit = (e) => { e.preventDefault(); };
+  f.onsubmit = (e) => {
+    e.preventDefault();
+  };
 }
+
+// Initializing top dropdown
 const drop = document.querySelector('.dropdown');
-const dropLink = document.querySelector('.header-link:nth-child(1)');
-dropLink.onmouseover = () => {
-  drop.classList.add('active')
+const links = document.querySelectorAll('.header-link');
+for (let l=1; l<links.length; l++) {
+  links[0].onclick = () => {
+    drop.classList.add('active')
+  }
+  links[l].onmouseover = () => {
+    drop.classList.remove('active')
+  }
 }
-drop.onmouseleave = () => {
-  drop.classList.remove('active')
+
+// Smooth scrolling
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
 }
+
